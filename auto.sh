16dcebcf66f349f4da0d13a74c8c3336242fced5
antonif <32664236+antonif@users.noreply.github.com>
2018-05-24 21:26:36 +0200

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -7,3 +7,7 @@ sudo apt-get -y install ./teamviewer_amd64.deb
 wget https://repo.skype.com/latest/skypeforlinux-64.deb
 sudo dpkg -i skypeforlinux-64.deb
 sudo apt -y install -f
+wget https://www.apachefriends.org/xampp-files/5.6.20/xampp-linux-x64-5.6.20-0-installer.run
+sudo su
+chmod +x xampp-linux-x64-5.6.20-0-installer.run
+./xampp-linux-x64-5.6.20-0-installer.run
